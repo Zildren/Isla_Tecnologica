91151b36ddc472d0df415d73e23061d36418a04e
Zildren <richy.pined4@gmail.com>
2026-03-24 22:09:57 -0600

    fix: 32y

--- backend/src/main/java/com/example/demo/controller/UsuarioController.java
+++ backend/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/usuarios")
 // 🟢 PERMITE LA CONEXIÓN DESDE REACT (Evita el error de CORS)
-@CrossOrigin(origins = "*") 
+@CrossOrigin(origins = "http://localhost:3000") 
 public class UsuarioController {
 
     @Autowired
--- backend/src/main/java/com/example/demo/controller/VentaController.java
+++ backend/src/main/java/com/example/demo/controller/VentaController.java
@@ -12,7 +12,6 @@ import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/ventas")
-@CrossOrigin(origins = "*") 
 public class VentaController {
 
     @Autowired
